b2cf1d0671eb5b3d1c1b6f3783f11742144cdc1f
Kazuya Erdos <kazuya_erdos@brown.edu>
2024-05-25 18:43:49 -0400

    feat. timing and stopping

--- tracker.py
+++ tracker.py
@@ -1,6 +1,7 @@
 import time
 import sys
 from datetime import date, datetime
+import keyboard
 
 #---------------------NOTES---------------------
 # print(date.today()) # get date in YYYY-MM-DD
@@ -13,15 +14,36 @@ from datetime import date, datetime
 def get_current_time_str():
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
-    return str(current_time)
+    return current_time
 
-def print_timer():
+def start_stop():
+    start_time = datetime.now()
     while True:
-        sys.stdout.write(f'\r{get_current_time_str()}')
+        if keyboard.is_pressed('space'):
+            break;
+        current_time = datetime.now()
+        sys.stdout.write(f'\r{format_timer(time_diff_in_seconds(start_time, current_time))}')
         sys.stdout.flush()
-        time.sleep(1)
+        time.sleep(0.1)
 
+def time_diff_in_seconds(start, stop):
+    time_delta = stop - start
+    return int(time_delta.total_seconds())
 
+def format_timer(num_seconds : int):
+    """
+    Given a number of seconds format to HH:MM:SS
+    """
+
+    def format_time(t):
+        return str(t) if t >= 10 else "0"+str(t)
+
+    hours = num_seconds // 3600
+    minutes = (num_seconds % 3600) // 60
+    seconds = num_seconds % 60
+    return format_time(hours) + ":" + format_time(minutes) + ":" + format_time(seconds)
+
+### Entry Point ###
 if __name__ == '__main__':
-    # print("hello world")
-    print_timer()
+    print("TASK: FOREVERFAN")
+    start_stop()
