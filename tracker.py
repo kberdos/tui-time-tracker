98363f80098bee1f16620f5acce203190d6f1e48
Kazuya Erdos <kazuya_erdos@brown.edu>
2024-05-25 19:22:18 -0400

    feat. stop and start, logging

--- tracker.py
+++ tracker.py
@@ -1,5 +1,6 @@
 import time
 import sys
+import os
 from datetime import date, datetime
 import keyboard
 
@@ -16,15 +17,27 @@ def get_current_time_str():
     current_time = now.strftime("%H:%M:%S")
     return current_time
 
-def start_stop():
+def run_timer():
+    print("running timer\n")
     start_time = datetime.now()
     while True:
-        if keyboard.is_pressed('space'):
-            break;
         current_time = datetime.now()
         sys.stdout.write(f'\r{format_timer(time_diff_in_seconds(start_time, current_time))}')
         sys.stdout.flush()
-        time.sleep(0.1)
+        if keyboard.is_pressed('space'):
+            stop_and_log(start_time, current_time)
+            break
+        time.sleep(0.01)
+
+def stop_and_log(start_time, current_time):
+    print(f'\nlogged {format_timer(time_diff_in_seconds(start_time, current_time))}')
+    cont = input("type s to continue: ")
+    if cont[-1] == "s":
+        os.system('clear')
+        run_timer()
+    else:
+        print("bruh")
+
 
 def time_diff_in_seconds(start, stop):
     time_delta = stop - start
@@ -46,4 +59,4 @@ def format_timer(num_seconds : int):
 ### Entry Point ###
 if __name__ == '__main__':
     print("TASK: FOREVERFAN")
-    start_stop()
+    run_timer()
